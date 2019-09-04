cfe1468f92935f71cbcc2a81715bc06bd1fb0ad1
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-04 11:41:45 -0400

    First Commit

---
+++ GitExample1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GitExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Taylor";
+            string lastName = "Hemphill";
+
+            int age = 28;
+
+            int puttingOnSomeYears = 40;
+
+            int oldAge = age + puttingOnSomeYears;
+
+            Console.WriteLine(firstName + " " + lastName);
+
+            Console.WriteLine("I am " + age +" years old");
+        
+            Console.WriteLine("I am glad i'm not " + oldAge + " year old");
+        }
+    }
+}
